0af097f13494ad4de07d875055c60b68ba3e2d56
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-04-22 16:51:57 -0230

    2022-04-22-3
    got egg to crack after bouncing.

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const egg = document.getElementById('egg');
 
 egg.addEventListener('click', (e) => {
     e.preventDefault();
-    crackEgg();
+    bounceEgg();
 })
-function crackEgg() {
-    document.getElementById('Crack').classList.remove("hide");
+function bounceEgg() {
+    document.getElementById('egg').classList.add("bounce");
+    crackEgg();
 }
+
+function crackEgg() {
+   const crack = function() {  
+   document.getElementById('Crack').classList.remove("hide"); 
+}
+ 
+setTimeout(crack, 1000); 
+}
+
