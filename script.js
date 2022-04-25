8dfdae792ab557a5888883bb177a8d30cf512821
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-04-25 11:20:41 -0230

    2022-04-25-1
    Made egg crack slowly on each bounce.

--- script.js
+++ script.js
@@ -6,14 +6,50 @@ egg.addEventListener('click', (e) => {
 })
 function bounceEgg() {
     document.getElementById('egg').classList.add("bounce");
-    crackEgg();
+    document.getElementById('Text').classList.add("hide");
+    crackEggOne();
+    crackEggTwo();
+    crackEggThree();
+    crackEggFour();
+    crackEggFive();
 }
 
-function crackEgg() {
+function crackEggOne() {
    const crack = function() {  
-   document.getElementById('Crack').classList.remove("hide"); 
+   document.getElementById('CrackOne').classList.remove("hide"); 
 }
  
 setTimeout(crack, 1000); 
 }
 
+function crackEggTwo() {
+    const crack = function() {  
+    document.getElementById('CrackTwo').classList.remove("hide"); 
+ }
+  
+ setTimeout(crack, 2000); 
+ }
+
+ function crackEggThree() {
+    const crack = function() {  
+    document.getElementById('CrackThree').classList.remove("hide"); 
+ }
+  
+ setTimeout(crack, 3000); 
+ }
+
+ function crackEggFour() {
+    const crack = function() {  
+    document.getElementById('CrackFour').classList.remove("hide"); 
+ }
+  
+ setTimeout(crack, 4000); 
+ }
+
+ function crackEggFive() {
+    const crack = function() {  
+    document.getElementById('CrackFive').classList.remove("hide"); 
+ }
+  
+ setTimeout(crack, 5000); 
+ }
