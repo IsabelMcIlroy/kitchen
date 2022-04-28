c27de4e5d6097ae3ff79e5bd80cdc9c54ed3c52f
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-04-28 09:48:26 -0230

    2022-04-28-1
    got time to display on oven clock.

--- kitchenScript.js
+++ kitchenScript.js
@@ -1,2 +1,17 @@
 // kitchen page
 const clock = document.getElementById('time');
+
+function formatTime(dateObject) {
+    const parts = {
+        hour: dateObject.getHours(),
+        minute: dateObject.getMinutes().toString().padStart(2, "0")
+    }
+    document.getElementById('clock').innerHTML = `${parts.hour}:${parts.minute}`
+    return `${parts.hour}:${parts.minute}`
+    
+} 
+
+const myTime = new Date();
+const myTimeFormatted = formatTime(myTime);
+
+console.log(myTimeFormatted);
